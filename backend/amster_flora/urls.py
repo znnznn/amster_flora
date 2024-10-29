9b36a642d001203d74c5e0a52bec745d60cbb3ae
znnznn <znnznn@ukr.net>
2024-10-29 10:22:39 +0200

    enable auth routers

--- backend/amster_flora/urls.py
+++ backend/amster_flora/urls.py
@@ -3,19 +3,20 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from users.views import UserViewSet
+from users.views import UserViewSet, ContactUsAPIView
 from .settings import ENVIRONMENT
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
+router.register('contact-us', ContactUsAPIView)
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('/', include(router.urls)),
-    # path('api/v1/token/', TokenObtainPairView.as_view(), name='token_create'),
-    # path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_create'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('amster_flora.doc_api')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
