c69ce2cad18cca705c091558604fb99f9e7af512
znnznn <znnznn@ukr.net>
2024-12-28 23:58:44 +0200

    add wayfor pay serializer and add correct response for wayforpay

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,3 +1,6 @@
+import hashlib
+import hmac
+import time
 from decimal import Decimal
 
 from django.shortcuts import render
@@ -8,7 +11,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.parsers import FormParser
 from rest_framework.response import Response
 
-from amster_flora.settings import LIQPAY_PUBLIC_KEY, LIQPAY_PRIVATE_KEY
+from amster_flora.settings import LIQPAY_PUBLIC_KEY, LIQPAY_PRIVATE_KEY, WAYFORPAY_SECRET_KEY
 from common.constants import PaymentStatus, Role
 from orders.models import Order
 from payments.models import Transaction
@@ -63,4 +66,29 @@ class WayForPayView(CreateAPIView):
     def create(self, request, *args, **kwargs):
         print('wayforpay')
         print(request.data)
-        return Response({'wayforpay': 'wayforpay'})
+        merchantSignature = request.data.get('merchantSignature')
+        data = dict(
+            merchantAccount=request.data.get('merchantAccount'),
+            orderReference=request.data.get('orderReference'),
+            amount=request.data.get('amount'),
+            currency=request.data.get('currency'),
+            authCode=request.data.get('authCode'),
+            cardPan=request.data.get('cardPan'),
+            transactionStatus=request.data.get('transactionStatus'),
+            reasonCode=request.data.get('reasonCode'),
+        )
+        values = [str(item) for item in data.values()]
+        print('data', data)
+        right_signature = hmac.new(WAYFORPAY_SECRET_KEY.encode(), ";".join(values).encode(), hashlib.md5).hexdigest()
+        print('right_signature', right_signature)
+        print('merchantSignature', merchantSignature)
+        if merchantSignature == right_signature:
+            print('signature is valid')
+        response = {
+            'orderReference': data.get('orderReference'),
+            'status': 'accept',
+            'time': time.time(),
+            'accept': '',
+        }
+        print('response', response)
+        return Response(response, status=status.HTTP_200_OK)
